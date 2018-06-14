aa469e7355c1fb0132b415a906c8e68b525d1fcc
onlyAngelia <479397375@qq.com>
2018-06-14 16:37:21 +0800

    add observer

--- YANotification.h
+++ YANotification.h
@@ -15,14 +15,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface YANotification : NSObject
 
-@property(nonnull, readonly, copy)NSString *name;
-@property(nonnull, readonly, retain)id object;
-@property(nonnull, readonly, copy)NSDictionary *userInfo;
+@property(nullable, readonly, copy)NSString *name;
+@property(nullable, readonly, retain)id object;
+@property(nullable, readonly, copy)NSDictionary *userInfo;
 
-- (instancetype)initWithName:(nonnull NSString *)name object:(nonnull id)object userInfo:(nonnull NSDictionary *)userInfo NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithName:(nullable NSString *)name object:(nullable id)object userInfo:(nullable NSDictionary *)userInfo NS_DESIGNATED_INITIALIZER;
 
 + (instancetype)initWithName:(NSString *)name object:(id)object;
-+ (instancetype)initWithName:(nonnull NSString *)name object:(nonnull id)object userInfo:(nonnull NSDictionary *)userInfo;
++ (instancetype)initWithName:(nullable NSString *)name object:(nullable id)object userInfo:(nullable NSDictionary *)userInfo;
 
 @end
 
@@ -32,6 +32,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (YANotificationCenter *)defaultNotificationCenter;
 
+- (void)postNotification:(YANotification *)notification;
+- (void)postNotificationName:(NSString *)aName object:(nullable id)anObject;
+- (void)postNotificationName:(NSString *)aName object:(nullable id)anObject userInfo:(nullable NSDictionary *)aUserInfo;
+
+- (void)removeObserver:(id)observer;
+- (void)removeObserver:(id)observer name:(nullable NSString *)aName object:(nullable id)anObject;
+
+- (id <NSObject>)addObserverForName:(nullable NSNotificationName)name object:(nullable id)obj queue:(nullable NSOperationQueue *)queue usingBlock:(void (^)(NSNotification *note))block API_AVAILABLE(macos(10.6), ios(4.0), watchos(2.0), tvos(9.0));
 @end
 
 NS_ASSUME_NONNULL_END
--- YANotification.m
+++ YANotification.m
@@ -8,11 +8,32 @@
 
 #import "YANotification.h"
 
+/*****************YANoticicationModel**********************/
+
+typedef void(^NotificationQueueBlock)(YANotification *notification);
+
+@interface YANotificationModel:NSObject
+
+@property (nullable, nonatomic, strong)id object;
+@property (nullable, nonatomic, assign)SEL selector;
+@property (nullable, nonatomic, strong)id observer;
+@property (nullable, nonatomic, copy)  NSString *notificationName;
+@property (nullable, nonatomic, strong)NSOperationQueue *queue;
+@property (nullable, nonatomic, copy)NotificationQueueBlock queueBlock;
+
+@end
+
+@implementation YANotificationModel
+
+
+@end
+
+/*****************YANoticication**********************/
 @interface YANotification()
 
-@property(nonnull, readwrite, copy)NSString *name;
-@property(nonnull, readwrite, retain)id object;
-@property(nonnull, readwrite, copy)NSDictionary *userInfo;
+@property(nullable, readwrite, copy)NSString *name;
+@property(nullable, readwrite, retain)id object;
+@property(nullable, readwrite, copy)NSDictionary *userInfo;
 
 @end
 
@@ -23,7 +44,7 @@
     return [self initWithName:@"" object:@"" userInfo:[NSDictionary dictionary]];
 }
 
-- (instancetype)initWithName:(nonnull NSString *)name object:(nonnull id)object userInfo:(nonnull NSDictionary *)userInfo
+- (instancetype)initWithName:(nullable NSString *)name object:(nullable id)object userInfo:(nullable NSDictionary *)userInfo
 {
     self = [super init];
     if (self) {
@@ -36,16 +57,17 @@
 
 + (instancetype)initWithName:(NSString *)name object:(id)object
 {
-    return [YANotification initWithName:name object:object userInfo:[NSDictionary dictionary]];
+    return [YANotification initWithName:name object:object userInfo:nil];
 }
 
-+ (instancetype)initWithName:(nonnull NSString *)name object:(nonnull id)object userInfo:(nonnull NSDictionary *)userInfo
++ (instancetype)initWithName:(nullable NSString *)name object:(nullable id)object userInfo:(nullable NSDictionary *)userInfo
 {
     return [YANotification initWithName:name object:object userInfo:userInfo];
 }
 
 @end
 
+/*****************YANotificationCenter**********************/
 
 @interface YANotificationCenter()
 
@@ -72,6 +94,63 @@
         _observersArray = [NSMutableArray array];
     }
     return self;
+}
+
+- (void)addObserver:(id)observer selector:(SEL)aSelector name:(nullable NSNotificationName)aName object:(nullable id)anObject
+{
+    YANotificationModel * notificationModel = [[YANotificationModel alloc] init];
+    notificationModel.observer = observer;
+    notificationModel.selector = aSelector;
+    notificationModel.notificationName = aName;
+    notificationModel.object = anObject;
+    [self.observersArray addObject:notificationModel];
+
+}
+
+- (void)postNotification:(YANotification *)notification
+{
+    
+}
+
+- (void)postNotificationName:(NSString *)aName object:(nullable id)anObject
+{
+    YANotification *notification = [[YANotification alloc] initWithName:aName object:anObject userInfo:nil];
+    [self postNotification:notification];
+    
+}
+
+- (void)postNotificationName:(NSString *)aName object:(nullable id)anObject userInfo:(nullable NSDictionary *)aUserInfo
+{
+    YANotification *notification = [[YANotification alloc] initWithName:aName object:anObject userInfo:aUserInfo];
+    [self postNotification:notification];
+    
+}
+
+- (void)removeObserver:(id)observer
+{
+    [self removeObserver:observer name:nil object:nil];
+}
+
+- (void)removeObserver:(id)observer name:(nullable NSString *)aName object:(nullable id)anObject
+{
+    __weak typeof(&*self)weakself = self;
+    [self.observersArray enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        
+        YANotificationModel *model = (YANotificationModel *)obj;
+        if (model.observer == observer && [model.notificationName isEqualToString:aName]&& model.object == anObject) {
+            [weakself.observersArray removeObjectAtIndex:idx];
+            *stop = YES;
+        }
+    }];
+}
+
+- (id <NSObject>)addObserverForName:(NSString *)name object:(id)obj queue:(NSOperationQueue *)queue usingBlock:(void(^)(YANotification * notification))block
+{
+    YANotificationModel *notificationModel = [[YANotificationModel alloc] init];
+    notificationModel.object = obj;
+    notificationModel.queue = queue;
+    notificationModel.queueBlock = block;
+    return nil;
 }
 
 @end
